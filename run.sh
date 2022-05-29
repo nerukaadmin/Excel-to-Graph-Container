fb31ecbd990ba1e50caec19f79c05577ac8949d5
Neruka Lakshitha <nerukaneo94@gmail.com>
2022-05-29 21:00:27 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo Pass argumrnt for script....
-echo For all "a"
-echo For team "t"
-read input </dev/tty
-docker build -t ex_to_graph:v1 .
-docker run -v /home/neo/Desktop/Project_graph/dokcer_con:/excel ex_to_graph:v1 -e 
