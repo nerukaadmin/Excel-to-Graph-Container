d9b01728cbebe41af3791da0cd776ab5a69d79af
neo <nerukaneo94@gmail.com>
2022-05-29 21:13:10 +0530

    Merge v3

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo Pass argumrnt for script....
-echo For all "a"
-echo For team "t"
-read input </dev/tty
-docker run -v /home/neo/Desktop/Project_graph/dokcer_con:/excel ex_to_graph:v1 -e 
