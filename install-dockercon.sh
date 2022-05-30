4a4e83716cac444bbe73d6b36b5c6d7133351070
neo <nerukaneo94@gmail.com>
2022-05-31 03:24:05 +0530

    Merge v16

--- install-dockercon.sh
+++ install-dockercon.sh
@@ -2,7 +2,7 @@
 echo Checking Docker..
 if docker --version ; then 
 	docker pull nerukaneo/ex_to_graph:v1
-	curl https://raw.githubusercontent.com/nerukaadmin/Excel-to-Graph-Container/main/init --output init
+	curl https://raw.githubusercontent.com/nerukaadmin/Excel-to-Graph-Container/main/init-con --output init
 	echo init Pulled.
 	mkdir -p  OUT
 	mkdir -p  IN
