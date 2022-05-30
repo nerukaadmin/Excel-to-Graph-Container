68487b2e058d39bcdd10c5d2d6aeb24f2a9e265b
neo <nerukaneo94@gmail.com>
2022-05-31 03:07:51 +0530

    Merge v12

---
+++ install-dockercon.sh
@@ -0,0 +1,20 @@
+!/bin/sh
+echo Checking Docker..
+if docker --version ; then 
+	pull nerukaneo/ex_to_graph:v1
+	mkdir -p  OUT
+	mkdir -p  IN
+	mkdir -p  tmp
+	chmod  -R 777 OUT
+	chmod  -R 777 IN
+	chmod  -R 777 tmp
+	touch team_member_list.txt
+	chmod 777 team_member_list.txt
+	echo Folder stucture completed...!
+	echo creating run.sh
+	cat init > run.sh
+	rm init 
+else
+	echo install Docker..
+	echo please visit https://docs.docker.com/engine/install/ubuntu/
+fi
